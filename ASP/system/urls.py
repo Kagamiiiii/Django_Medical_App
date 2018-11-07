02550fc7f1a009cf5ba7b53973fa42cf58667532
Kagamiiiii <tsouyingai@gmail.com>
2018-11-07 10:10:01 +0800

    fixed url problem and database problem

--- ASP/system/urls.py
+++ ASP/system/urls.py
@@ -14,15 +14,9 @@ app_name = "system"
 urlpatterns = [
     # path('', views.index, name='index'),
     # to use generic view, only primary key can be included in the url
-    path('detail/<int:pk>/', views.DetailView.as_view(), name='detail'),
     # noted there maybe some requests with parameters in the link,
     # for example 4/detail. In this case, the <int:pk> clause matches the integer
     # and passes that as an argument to the function DetailView in views.py so they can handle them.
     # the name option, however, is the assign a name for such redirection, so in other files you can
     # use that abbreviation to represent this.
-    path('dispatch/', views.DispatchView.as_view(), name='dispatch'),
-    path('<str:cat>', views.displayByCategory, name='displayByCategory'),
-    path('order/<int:orderID>/', views.orderView, name='order'),
-    path('clinic_manager/create_order/', views.createOrder2, name='create_order'),
-    path('dispatcher/dispatchUpdate/', views.DispatchView.as_view(), name='dispatch_update'),
 ]
