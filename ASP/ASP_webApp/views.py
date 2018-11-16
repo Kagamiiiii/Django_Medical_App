be236e566b99c0d97ca1530e181c5ebc2b068393
kentpun <kent@hkcup.com>
2018-11-16 23:53:00 +0800

    [MINOR] debug again

--- ASP/ASP_webApp/views.py
+++ ASP/ASP_webApp/views.py
@@ -102,82 +102,87 @@ class createOrderPage(View):
 # ---------------------------------------------------------------------
 
 # use return a json containing all the orders that are "Queued for Dispatch"
-def dispatchView(request):
-    result = Order.objects.filter(status="Queued for Dispatch")\
-                                    .values('id', 'name', 'priority', 'ordering_clinic', 'weight')\
-                                    .order_by('priority', 'orderedDatetime', 'id')
-    json_result = []
-    max_weight = 25.0
-    for item in result:
-        max_weight -= item.weight
-        if max_weight < 0:
-            break
-        single_order = {}
-        single_order["order_id"] = item.id
-        single_order["order_name"] = item.name
-        single_order["priority"] = item.priority
-        single_order["clinic"] = item.ordering_clinic
-        single_order["weight"] = item.weight
-        order_items = Include.objects.filter(order=item.id).values('supply', 'quantity')
-        # get all supplies of the corresponding order
-        children = []
-        for info in order_items:
-            item_name = Supply.objects.get(id=info.supply).values('name')
-            children.append({"name" : item_name, "quantity" : info.quantity})
-        single_order["children"] = children
-        json_result.append(single_order)
+class DispatchPage(View):
 
-    return render_to_response("Dispatcher/dispatch.html", {'results': json_result})
-
-
-
-# update status and dispatch datetime of all selected orders
-def dispatchUpdate(request, orders):
-    for order_id in orders:
-        singleOrder = Order.objects.filter(id=order_id).update(status="Dispatched")
-        dateTime = timezone.now()
-        singleOrder.objects.update(dispatchedDatetime=dateTime)
-        singleOrder.save()
-    return render(request, "Dispatcher/dispatch.html", {'message' : 'success'})
-
-# def createItinerary(self):
-# create itinerary file
-# orders should be a list of order idss
-def createItinerary(request, orders):
-   hospitalName = 'Queen Mary Hospital Drone Port'
-   # sets the hospital's id as first location
-   hospital_location = Location.objects.get(name=hospitalName)
-   location_id = hospital_location.pk
-   order_ids = orders.copy()
-   items = []
-   # check sequence for locations
-   while order_ids:
-       min = 999999
-       temp = None
-       for order_id in order_ids:
-           destination = Order.objects.get(id=order_id).location
-           d = Distance.objects.get(distanceFrom=location_id, distanceTo=destination).distance
-           if d < min:
-               temp = order_id
-               min = d
-       location_id = temp
-       order_ids.remove(temp)
-       cur_location = Location.objects.get(id=temp)
-       item = { 'name' : cur_location.name,
-                'latitude' : cur_location.latitude,
-                'longitude' : cur_location.longitude,
-                'altitude' : cur_location.altitude }
+    def dispatchView(request):
+        result = Order.objects.filter(status="Queued for Dispatch")\
+                                        .values('id', 'name', 'priority', 'ordering_clinic', 'weight')\
+                                        .order_by('priority', 'orderedDatetime', 'id')
+        json_result = []
+        max_weight = 25.0
+        for item in result:
+            max_weight -= item.weight
+            if max_weight < 0:
+                break
+            single_order = {}
+            single_order["order_id"] = item.id
+            single_order["order_name"] = item.name
+            single_order["priority"] = item.priority
+            single_order["clinic"] = item.ordering_clinic
+            single_order["weight"] = item.weight
+            order_items = Include.objects.filter(order=item.id).values('supply', 'quantity')
+            # get all supplies of the corresponding order
+            children = []
+            for info in order_items:
+                item_name = Supply.objects.get(id=info.supply).values('name')
+                children.append({"name" : item_name, "quantity" : info.quantity})
+            single_order["children"] = children
+            json_result.append(single_order)
+    
+        return render_to_response("Dispatcher/dispatch.html", {'results': json_result})
+    
+    
+    
+    # update status and dispatch datetime of all selected orders
+    def dispatchUpdate(request):
+        orders = request.POST.get("orderSet")
+        for order_id in orders:
+            singleOrder = Order.objects.filter(id=order_id).update(status="Dispatched")
+            dateTime = timezone.now()
+            singleOrder.objects.update(dispatchedDatetime=dateTime)
+            singleOrder.save()
+        return render(request, "Dispatcher/dispatch.html", {'message' : 'success'})
+    
+    # def createItinerary(self):
+    # create itinerary file
+    # orders should be a list of order idss
+    def createItinerary(request):
+       orders = request.POST.get("orderSet")
+       hospitalName = 'Queen Mary Hospital Drone Port'
+       # sets the hospital's id as first location
+       hospital_location = Location.objects.get(name=hospitalName)
+       location_id = hospital_location.pk
+       order_ids = orders.copy()
+       items = []
+       # check sequence for locations
+       while order_ids:
+           min = 999999
+           temp = None
+           for order_id in order_ids:
+               destination = Order.objects.get(id=order_id).location
+               d = Distance.objects.get(distanceFrom=location_id, distanceTo=destination).distance
+               if d < min:
+                   temp = order_id
+                   min = d
+           location_id = temp
+           order_ids.remove(temp)
+           cur_location = Location.objects.get(id=temp)
+           item = { 'name' : cur_location.name,
+                    'latitude' : cur_location.latitude,
+                    'longitude' : cur_location.longitude,
+                    'altitude' : cur_location.altitude }
+           items.append(item)
+       item = { 'name' : 'Queen Mary Hospital Drone Port',
+                'latitude' : hospital_location.latitude,
+                'longitude' : hospital_location.longitude,
+                'altitude' : hospital_location.altitude }
        items.append(item)
-   item = { 'name' : 'Queen Mary Hospital Drone Port',
-            'latitude' : hospital_location.latitude,
-            'longitude' : hospital_location.longitude,
-            'altitude' : hospital_location.altitude }
-   items.append(item)
-   return render(request, "Dispatcher/dispatch.html", {'results': items})
+       return render(request, "Dispatcher/dispatch.html", {'results': items})
 
 # ---------------------------WarehousePersonnel------------------------
 # ---------------------------------------------------------------------
 class WarehouseView(generic.ListView):
+
     context_object_name = 'warehouseList'
     # equal to pass a list
     # {orderList: queryset}
@@ -190,46 +195,46 @@ class WarehouseView(generic.ListView):
 
     # remove order from the top to pick and pack (change status to "processing by warehouse")
     # and return the details of the selected order
-def orderSelect(request):
-    chosen = Order.objects.filter(status="Queued for Processing").order_by('priority', 'orderedDatetime', 'id')[:1]
-    chosen.objects.update(status="Processing by Warehouse")
-    chosen.save()
-    jsonresult = []
-    jsonresult.append(chosen)
-    return render(request, "WHP/warehouse.html", {'results': jsonresult})
+    def orderSelect(request):
+        chosen = Order.objects.filter(status="Queued for Processing").order_by('priority', 'orderedDatetime', 'id')[:1]
+        chosen.objects.update(status="Processing by Warehouse")
+        chosen.save()
+        jsonresult = []
+        jsonresult.append(chosen)
+        return render(request, "WHP/warehouse.html", {'results': jsonresult})
 
 
-    # get a shipping label consists of (order_id, supplies name, quantity, priority, destination name)
-    # and update status of the selcted order (status ==> "Queued for Dispatch")
-def getShippingLabel(request, order_id):
-    order_selected = Order.objects.filter(id=order_id)
-    items = Include.objects.filter(order=order_id)
-    quantity = 0
-    for item in items:
-        quantity += item.quantity
-    buffer =  io.BytesIO()
-    pdf = canvas.Canvas(buffer)
-    pdf.setLineWidth(.3)
-    pdf.setFont('Helvetica', 12)
+       # get a shipping label consists of (order_id, supplies name, quantity, priority, destination name)
+       # and update status of the selcted order (status ==> "Queued for Dispatch")
+    def getShippingLabel(request, order_id):
+        order_selected = Order.objects.filter(id=order_id)
+        items = Include.objects.filter(order=order_id)
+        quantity = 0
+        for item in items:
+            quantity += item.quantity
+        buffer =  io.BytesIO()
+        pdf = canvas.Canvas(buffer)
+        pdf.setLineWidth(.3)
+        pdf.setFont('Helvetica', 12)
 
-    pdf.drawString(30, 750, 'Queen Mary ')
-    pdf.drawString(30, 735, 'Hospital Drone Port')
-    pdf.drawString(450, 750, 'Order id:')
-    pdf.drawString(500, 750, order_id)
+        pdf.drawString(30, 750, 'Queen Mary ')
+        pdf.drawString(30, 735, 'Hospital Drone Port')
+        pdf.drawString(450, 750, 'Order id:')
+        pdf.drawString(500, 750, order_id)
 
-    pdf.line(480, 747, 580, 747)
+        pdf.line(480, 747, 580, 747)
 
-    pdf.drawString(275, 725, 'Quantity:')
-    pdf.drawString(500, 725, quantity)
-    pdf.line(378, 723, 580, 723)
+        pdf.drawString(275, 725, 'Quantity:')
+        pdf.drawString(500, 725, quantity)
+        pdf.line(378, 723, 580, 723)
 
-    pdf.drawString(30, 703, 'RECEIVED BY:')
-    pdf.line(120, 700, 580, 700)
-    pdf.drawString(120, 703, order_selected.name)
-    pdf.drawString(450, 703, 'Priority:')
-    pdf.drawString(500, 703, order_selected.priority)
-    pdf.showPage()
-    pdf.save()
-    order_selected.objects.update(status="Queued for Dispatch")
-    order_selected.save()
-    return FileResponse(buffer, as_attachment=True, filename='shipping_label.pdf')
+        pdf.drawString(30, 703, 'RECEIVED BY:')
+        pdf.line(120, 700, 580, 700)
+        pdf.drawString(120, 703, order_selected.name)
+        pdf.drawString(450, 703, 'Priority:')
+        pdf.drawString(500, 703, order_selected.priority)
+        pdf.showPage()
+        pdf.save()
+        order_selected.objects.update(status="Queued for Dispatch")
+        order_selected.save()
+        return FileResponse(buffer, as_attachment=True, filename='shipping_label.pdf')
