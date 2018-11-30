6b52bcba8a8c7dc4f67dda126a2aa47042ab7dbf
Kagamiiiii <tsouyingai@gmail.com>
2018-11-30 15:42:45 +0800

    [CONSTRUCTION 2][MERGED] Views changed and optimizing

--- ASP/ASP_webApp/static/js/dispatch.js
+++ ASP/ASP_webApp/static/js/dispatch.js
@@ -59,17 +59,4 @@ function updateToDispatch(){
           }
         }
     );
-    $.ajax({
-          type: "POST",
-          url: "./sendEmail/",
-          data: {
-            item: window.items,
-          },
-          traditional: true,
-          success: function (respond) {
-              alert("email sent to corresponding clinic manager.");
-              // location.reload();
-          }
-        }
-    );
 }
